274cc0200f29d0184f46de63122098d3e7ff077a
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2025-03-31 23:20:18 +0530

    feat(update): Google Signin button

--- components/GoogleLogin/GoogleBtn.tsx
+++ components/GoogleLogin/GoogleBtn.tsx
@@ -1,12 +1,19 @@
-import styles from './GoogleBtn.module.css'
 import React from "react";
-import {useAuth} from "@/contexts/auth-context";
-
+import { useAuth } from "@/contexts/auth-context";
 
 export const GoogleBtn: React.FC = () => {
-    const {signInWithGoogle} = useAuth();
-    return <button className={styles.GoogleBtn} onClick={signInWithGoogle}><img
-        src="https://static-00.iconduck.com/assets.00/google-icon-2048x2048-czn3g8x8.png" alt=""/>
-        Sign in with Google
-    </button>;
+    const { signInWithGoogle } = useAuth();
+    return (
+        <button
+            onClick={signInWithGoogle}
+            className="flex items-center justify-center mt-2 px-4 py-2 bg-white border border-gray-300 rounded-lg shadow-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+            <img
+                src="https://static-00.iconduck.com/assets.00/google-icon-2048x2048-czn3g8x8.png"
+                alt="Google Icon"
+                className="w-6 h-6 mr-2"
+            />
+            Sign in with Google
+        </button>
+    );
 };
