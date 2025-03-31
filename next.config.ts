ee50c9cdd89dd523ba616abe8b83b1e4bebbf329
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2025-03-31 18:36:01 +0530

    feat(added): image domains to config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images:{
+        domains: ["vn06itx8ks.ufs.sh","utfs.io","lh3.googleusercontent.com","picsum.photos"]
+    }
 };
 
 export default nextConfig;
